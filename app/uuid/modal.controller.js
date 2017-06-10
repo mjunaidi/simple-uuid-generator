99204221ce5fd57160611901aea569d297cb873f
junaidi.jusoh@daimler.com <junaidi.jusoh@daimler.com>
2017-06-10 15:24:04 +0800

    Free update, fix or changes.

--- app/uuid/modal.controller.js
+++ app/uuid/modal.controller.js
@@ -3,11 +3,11 @@
 
   angular.module('app.model').controller('ModalController', ModalController);
 
-  ModalController.$inject = [ 'modelService', '$modalInstance', 'parentCtrl', 'config' ];
+  ModalController.$inject = [ 'modelService', '$uibModalInstance', 'parentCtrl', 'config' ];
 
-  function ModalController(modelService, modalInstance, parentCtrl, config) {
+  function ModalController(modelService, uibModalInstance, parentCtrl, config) {
     this._modelService = modelService;
-    this._modalInstance = modalInstance;
+    this._modalInstance = uibModalInstance;
     this.parentCtrl = parentCtrl;
     if (typeof config !== 'undefined' && config !== null) {
       this._modelService.init(this, config);
